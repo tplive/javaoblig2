6a4c215130f7f67ff05dc01f3b6df057f2183437
Thomas <Thomas@SARUMAN>
2016-11-26 19:17:59 +0100

    Flyttet mock-data til egen metode. Lagt inn tom dataFile.txt, lagt inn Save og Cancel buttons + handlers

---
+++ src/no/norduni/javaoblig2/EditFlightController.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.norduni.javaoblig2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Thomas
+ */
+public class EditFlightController implements Initializable {
+
+    @FXML
+    private TextField tfFlightNo;
+    @FXML
+    private TextField tfFraFlyplass;
+    @FXML
+    private TextField tfTilFlyplass;
+    @FXML
+    private TextField tfStartTid;
+    @FXML
+    private TextField tfReiseTid;
+    @FXML
+    private TextField tfAntallSeter;
+    @FXML
+    private Button btSaveFlight;
+    @FXML
+    private Button btCancel;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }    
+
+    @FXML
+    private void btSaveFlightClick(ActionEvent event) {
+        
+        Flight flight = new Flight();
+        flight.setFlightNo(tfFlightNo.getText());
+        flight.setTilFlyplass(tfFraFlyplass.getText());
+        flight.setFraFlyplass(tfTilFlyplass.getText());
+        flight.setStartTid(tfStartTid.getText());
+        flight.setReiseTid(Integer.parseInt(tfReiseTid.getText()));
+        flight.setAntallSeter(Integer.parseInt(tfAntallSeter.getText()));
+        
+        //flights.add(flight);
+    }
+
+    @FXML
+    private void btCancelClick(ActionEvent event) {
+    }
+    
+}
--- src/no/norduni/javaoblig2/FXMLMainDocumentController.java
+++ src/no/norduni/javaoblig2/FXMLMainDocumentController.java
@@ -22,17 +22,20 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
  *
  * @author Thomas
  * 
- * Change to a different FXML document: http://www.javafxtutorials.com/tutorials/switching-to-different-screens-in-javafx-and-fxml/
  * 
  */
 public class FXMLMainDocumentController implements Initializable {
+    
+    String dataFile = "datafile.txt";
     
     @FXML
     private TableView tblViewFlights;
@@ -57,37 +60,7 @@ public class FXMLMainDocumentController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
-        Flight flight = new Flight();
-        flight.setFlightNo("WF1233");
-        flight.setTilFlyplass("BNN");
-        flight.setFraFlyplass("MQN");
-        flight.setStartTid("2016/12/20");
-        flight.setReiseTid(55);
-        flight.setAntallSeter(23);
-        
-        flights.add(flight);
-        
-        Reisende reisende = new Reisende();
-        reisende.setNavn("Eline Westerberg");
-        reisende.setKjonn("K");
-        reisende.setAlder(28);
-        reisende.setPassNo("123123456");
-        reisende.setGruppeKode(1);
-        
-        passasjerListe.add(reisende);
-        
-        Gruppe gruppe = new Gruppe();
-        gruppe.setFlightNo("WF1233");
-        gruppe.setGruppeKode(1);
-        
-        grupper.add(gruppe);
-        
-        Betalinger betaling = new Betalinger();
-        betaling.setPersonPassNo(123123456);
-        betaling.setBetalingsMaate(0);
-        betaling.setSum(2500.50);
-        
-        betalinger.add(betaling);
+        this.lastInnData();
         
         this.tblViewFlights.setItems(flights); 
         this.tblViewReisende.setItems(passasjerListe);
@@ -115,17 +88,21 @@ public class FXMLMainDocumentController implements Initializable {
             }
         
         
-    }    
+    }
+    
+    
 
     @FXML
     private void btClickNyFlight(ActionEvent event) throws IOException {
         
         try {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("EditFlight.fxml"));
-                Parent root1 = (Parent) fxmlLoader.load();
-                Stage stageEdit = new Stage();
-                stageEdit.setScene(new Scene(root1));  
-                stageEdit.show();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("EditFlight.fxml"));
+            
+            Stage stage = new Stage(StageStyle.DECORATED);
+            stage.setScene(new Scene((Pane) fxmlLoader.load()));
+            stage.setTitle("Edit Flight");
+            
+            stage.show();
         } catch(IOException e) {
            e.printStackTrace();
           }
@@ -133,5 +110,46 @@ public class FXMLMainDocumentController implements Initializable {
  
     }
 
+      private void lastInnData() {
+        try {
+            bookingDataStore.readFile(dataFile);
+            
+        } catch (Exception e) {
+            System.out.println(e);
+            
+            Flight flight = new Flight();
+            flight.setFlightNo("WF1233");
+            flight.setTilFlyplass("BNN");
+            flight.setFraFlyplass("MQN");
+            flight.setStartTid("2016/12/20");
+            flight.setReiseTid(55);
+            flight.setAntallSeter(23);
+
+
+            Reisende reisende = new Reisende();
+            reisende.setNavn("Eline Westerberg");
+            reisende.setKjonn("K");
+            reisende.setAlder(28);
+            reisende.setPassNo("123123456");
+            reisende.setGruppeKode(1);
+            
+            
+            Gruppe gruppe = new Gruppe();
+            gruppe.setFlightNo("WF1233");
+            gruppe.setGruppeKode(1);
+            
+            Betalinger betaling = new Betalinger();
+            betaling.setPersonPassNo(123123456);
+            betaling.setBetalingsMaate(0);
+            betaling.setSum(2500.50);
+            
+            this.flights.add(flight);
+            this.passasjerListe.add(reisende);
+            this.grupper.add(gruppe);
+            this.betalinger.add(betaling);
+            
+            
+        }
+    }
     
 }
